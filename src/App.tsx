3f01f81a1f6dc5d1c070418ac6683e8801234245
Anton Vlasov <nanostet@gmail.com>
2021-07-27 22:15:53 +0700

    Add single file version

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { ReactComponent as Logo } from './screenshots/appstore/0_APP_IPHONE_55_0.svg';
 import './App.css';
+import { useState } from 'react';
+import chroma from 'chroma-js'
 
-function App() {
+function App(props: any) {
+  const [color, setColor] = useState(new URLSearchParams(window.location.search).get("color") ?? '#13718A')
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input value={color} type="text" onChange={e => {
+          setColor(e.target.value)
+        }} />
+        <Logo height={500} fill={parseColor(color)} />
       </header>
     </div>
   );
+}
+
+const parseColor = (value: string) => {
+  try {
+    return chroma(value).hex()
+  } catch(e) {
+    return value
+  }
 }
 
 export default App;
